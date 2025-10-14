0bdf4e9751e553fd5a3227e11e02800246a4ea9b
Fassam Khan <muhammadfassam465@gmail.com>
2025-10-14 22:29:50 +0500

    changes

---
+++ app.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    let x = document.getElementById("menue");
+    if(x.style.display === "inline"){
+        x.style.display === "none";
+    }
+    else{
+        x.style.display = "inline"
+    }
+
+}
